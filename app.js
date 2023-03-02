8eca0c5c0d790582c7200729dee692643094fd35
Jeffrey <562475805lanu@gmail.com>
2023-03-02 22:30:17 +1100

    more error handling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const morgan = require('morgan');
+const express = require('express');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -29,12 +29,30 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// has to in the end of routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = '404';
+
+  next(err);
+  // will skip all other middle ware and goes into error handler
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
-// has to in the end of routes
 
 module.exports = app;
