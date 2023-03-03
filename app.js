34aa0dd018304c48e9fe5162b72562395fcfc21c
Jeffrey <562475805lanu@gmail.com>
2023-03-03 16:32:48 +1100

    global error handler module

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const morgan = require('morgan');
 const express = require('express');
 
+const appErr = require('./utils/appError');
+const globalErrHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -31,28 +34,16 @@ app.use('/api/v1/users', userRouter);
 
 // has to in the end of routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-  // });
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = '404';
 
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = '404';
+  next(new appErr(`Can't find ${req.originalUrl} on this server!`));
 
   next(err);
   // will skip all other middle ware and goes into error handler
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrHandler);
 
 module.exports = app;
