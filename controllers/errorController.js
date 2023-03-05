11842e4422ffe8189c1dd3dac768d6b9ba413577
Jeffrey <562475805lanu@gmail.com>
2023-03-05 16:57:32 +1100

    handling errors of development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,37 @@
-module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
+const sendErrorDev = () => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+const senErrorProd = () => {
+  // Operational, trusted error: send a message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't show error details
+  } else {
+    console.error('ERROR 💥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'something goes wrong',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev();
+  } else if (process.env.NODE_ENV === 'production') {
+    senErrorProd();
+  }
 };
