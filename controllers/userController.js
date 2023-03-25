d0ad70fab7dcaee70172d310739d6a423b5c593d
Jeffrey <562475805lanu@gmail.com>
2023-03-25 17:59:01 +1100

    add user update detail function

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,14 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res) => {
   let users = await User.find();
 
@@ -13,6 +21,26 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     data: {
       users,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1 create error if user send password through
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates', 400));
+  }
+  // 2 filter out the data we don't want
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3 Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: { user: updatedUser },
   });
 });
 
