1286f66b27b2173b2df82033485d2740d4784978
Jeffrey <562475805lanu@gmail.com>
2023-03-31 16:54:51 +1100

    update schema (with embed data)

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -24,10 +24,7 @@ mongoose
   .then(() => console.log('DB connection successful'));
 
 // Read JSON file
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // Import data into DB
 const importData = async () => {
