c269e61f2bd60e1d0321590e2266f7989bf072eb
Jeffrey <562475805lanu@gmail.com>
2023-03-31 17:42:43 +1100

    embed data with Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 
 // create a new schema
 // states out required or not, has default value or not, etc
@@ -110,6 +111,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
 
   {
@@ -134,6 +136,12 @@ tourSchema.pre('save', function (next) {
   // this -- is current processing document
   // console.log(this) -- {name : ...  slug:NA}
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
