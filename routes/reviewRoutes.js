f22597af331bd8604466de57882f3f65de71674f
Jeffrey <562475805lanu@gmail.com>
2023-04-10 18:22:43 +1000

    add auth

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,6 +4,8 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 // POST /reviews
 // POST /tour/1234sff/reviews
 // both go through ('/')
@@ -21,7 +23,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
