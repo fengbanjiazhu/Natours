87d9fb2c60d24c032068c85144d3c260fba30623
Jeffrey <562475805lanu@gmail.com>
2023-04-10 18:00:14 +1000

    add auth

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,34 +1,43 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+// const reviewController = require('../controllers/reviewController');
 const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
 
 router.use('/:tourId/reviews', reviewRouter);
 
-// added a middle ware before we reach getAllTour()
-// this middle ware -- aliasTopTours()
-// pre-set some query options in
-// help us with find top rating and sort by price
-// then uses getAllTour() with the query to get result
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTour);
 
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTour)
-  .post(tourController.createTour);
+  .get(tourController.getAllTour)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
 
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
   .delete(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
