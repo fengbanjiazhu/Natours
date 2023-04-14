c1f2695fb9baf9a23c197b8045796d7de3923847
Jeffrey <562475805lanu@gmail.com>
2023-04-14 18:36:32 +1000

    implement views router and controller

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -72,12 +73,8 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
+
+app.use('/', viewRouter);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
