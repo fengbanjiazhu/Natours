6af9d4fcd49f739e885447571b76fffe3f3182d4
Jeffrey <562475805lanu@gmail.com>
2023-04-16 21:56:57 +1000

    having problems of implementing leaflet map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,2 +1,6 @@
+// import L from '../../node_modules/leaflet/dist/leaflet.js';
+
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+let map = L.map('map').setView([51.505, -0.09], 13);
