a29e11b7a3dac829131e7499e38e2f4899f8b13b
Jeffrey <562475805lanu@gmail.com>
2023-04-17 20:44:58 +1000

    added map function

--- app.js
+++ app.js
@@ -27,7 +27,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      'img-src': ["'self'", 'https: data:'],
+      'script-src': ["'self'", 'https: data:'],
+    },
+  })
+);
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,6 +1,25 @@
-// import L from '../../node_modules/leaflet/dist/leaflet.js';
+const locationData = JSON.parse(
+  document.getElementById('map').dataset.locations
+);
+// console.log(locationData);
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+const startLocation = locationData[0].coordinates;
 
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([startLocation[1], startLocation[0]], 8);
+L.marker([startLocation[1], startLocation[0]]).addTo(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const locationCoords = locationData[1];
+
+locationCoords.forEach((el, i) => {
+  const coords = el.coordinates;
+  L.popup()
+    .setLatLng([coords[1], coords[0]])
+    .setContent(`Day ${i + 1} : ${el.description}`)
+    .addTo(map);
+});
