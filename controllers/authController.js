33e134294300a4b8f7b87a4009d5f619b8b3887a
Jeffrey <562475805lanu@gmail.com>
2023-04-18 21:40:38 +1000

    now token could set with cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,7 +79,10 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
+
   if (!token) {
     return next(new AppError('Please log in first', 401));
   }
