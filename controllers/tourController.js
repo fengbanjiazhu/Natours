45f84dfc29e244e4436d7723af2a6173367ac3e1
Jeffrey <562475805lanu@gmail.com>
2023-04-23 13:51:16 +1000

    fix resize middle ware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,33 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
+const multer = require('multer');
+const sharp = require('sharp');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Please upload image only', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+exports.resizeTourImages = (req, res, next) => {
+  // console.log(req.files);
+  next();
+};
 
 exports.getAllTour = factory.getAll(Tour);
 
