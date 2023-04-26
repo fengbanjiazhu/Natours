e38094c733543ae5960e2cc2688b9007d82aa5af
Jeffrey <562475805lanu@gmail.com>
2023-04-26 13:02:47 +1000

    implement test email sending (nodeMailer)

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -7080,7 +7080,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60468" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57737" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
